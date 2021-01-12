8ae141f7e9ec102d745179c6870978e2b67d8300
samielatri <samielatri.pro@gmail.com>
2021-01-12 15:19:16 +0100

    add backend api

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,15 @@ const socket = io();
 socket.on('message', message=>{
     console.log(message);
 });
+
+
+// User played his turn
+//something.addEventListner('submit', e=>{
+//    e.preventDefault();
+
+//    // get gameStatus
+//    const gameStatus = e.smthg.value;
+
+//    console.log(gameStatus);
+//    //Emit the game status to server
+//});
