0b058b3ad9c5992f04ca1cdf8a47684455ff51d2
samielatri <samielatri.pro@gmail.com>
2021-01-14 16:17:40 +0100

    update config

--- config/db.js
+++ config/db.js
@@ -1,4 +1,14 @@
 const mogoose = require('mongoose'); // mongodb interaction
+const project_name = 'pylos';
+const user_name = 'herpes';
+const __PASSWORD__ = require('./pwd.js');
+const pwd = encodeURIComponent(__PASSWORD__);
+const MongoClient = require('mongodb').MongoClient;
+
+// TODO : put everything in config
+// mongodb+srv://herpes:wSdPOFFQR1LnpLMw@pylos.j4ui7.mongodb.net/pylos?retryWrites=true&w=majority
+// mongodb+srv://herpes:<password>@pylos.j4ui7.mongodb.net/<dbname>?retryWrites=true&w=majority
+const uri = "mongodb+srv://herpes:"+ pwd +"@pylos.j4ui7.mongodb.net/?retryWrites=true&w=majority"; // actual online uri
 
 // connection to database
 const connectDB = async ()=>{
