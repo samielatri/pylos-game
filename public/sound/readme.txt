f397ec18fec7afce9f20890494bb50f2b2757d10
samielatri <samielatri.pro@gmail.com>
2021-01-14 15:36:04 +0100

    add music

---
+++ public/sound/readme.txt
@@ -0,0 +1,5 @@
+Music from :
+ https://www.bensound.com
+
+FREE License with Attribution
+You can use this music for free in your multimedia project (online videos (Youtube, Facebook,...), websites, animations, etc.) as long as you credit Bensound.com (in the description for a video).
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const hbs = require('express-handlebars');
+const session = require('express-session');
+const passport = require('passport');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 const dotenv = require('dotenv'); // env file
@@ -34,20 +37,42 @@ const app = express();
 app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true })); // TODO : set it back to false ?
 app.use(cookieParser());
+app.use(session({ secret: 'secret pylos',resave:true,saveUninitialized: true }));
 
 // satatic
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 // view engine setup
+
+// ejs
 app.set('view engine', 'ejs'); //set views ejs
 app.set('views', path.join(__dirname, 'views')); // set views directory
 
-// const trnasactions = require('./routes/transactions');
+// hbs
+//app.engine('hbs', hbs({
+//  extname: 'hbs',
+//  defaultLayout: 'default',
+//  layoutsDir: __dirname + '/views/',
+//}));
+//app.set('view engine', 'hbs');
+
+
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 const signup = require('./routes/user/signup');
 const login = require('./routes/user/login');
+
+// to uncomment
+//app.use('/', require('./server/users'));
+//app.use('/', require('./server/p'));
+//app.use('/', require('./server/passport'));
+
 
 // TODO : call it
 //const games = require('./routes/game/game');
@@ -60,22 +85,21 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); // for logs
 }
 
-// app.use('/api/v1/transactions', trnasactions);
+// app.use('/transactions', trnasactions);
 app.use(signup);
 app.use(login);
 // app.use('',games);
 
 
-// Configuration for deploymnet of on server and run on one port
-// TODO : change it to normal build, this one is for REACT
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+// if react build (Configuration for deploymnet of on server and run on one port)
+//if (process.env.NODE_ENV === 'production') {
+//    app.use(express.static('client/build'));
+//    app.get('*', (req, res) => {
+//        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//    });
+//}
 
-const DEFAULTPORT = 3000;
+const DEFAULTPORT = 5000; 
 const PORT = process.env.PORT || DEFAULTPORT;
 // listen for requests
 app.listen(PORT, console.log(`Server running  in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
